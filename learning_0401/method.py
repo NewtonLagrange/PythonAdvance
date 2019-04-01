66ec81d14123f6c38434208d14f98f38cd144c88
outime <903095546@qq.com>
2019-04-01 17:50:55 +0800

    动态添加类方法, 实例方法, 静态方法

--- learning_0401/method.py
+++ learning_0401/method.py
@@ -1,9 +1,13 @@
 """
 类方法, 实例方法, 静态方法
 """
+import types
 
 
 class Bird:
+    """
+    这是一只鸟
+    """
     def __init__(self, name):
         self._name = name
 
@@ -32,6 +36,30 @@ class Bird:
 
 
 bird = Bird('燕子')
-bird.fly()
-print(bird.name)
-Bird.fly()
+
+
+# 动态添加类方法
+@classmethod
+def fact(cls):
+    print('动态添加类方法')
+
+
+# 动态添加实例方法
+def fact1(self):
+    print('动态添加实例方法')
+
+
+# 动态添加静态方法
+@staticmethod
+def static():
+    print('这是一个静态方法')
+
+
+Bird.fact = fact
+bird.fact1 = types.MethodType(fact1, bird)
+Bird.static = static
+
+Bird.fact()
+bird.fact1()
+Bird.static()
+bird.static()
